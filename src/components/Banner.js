68430acafc56daec30359d93552d830a20b9470f
vedantkesharia <keshariavedant@gmail.com>
2023-08-07 19:36:21 +0530

    commit 7th aug

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,12 +2,14 @@ import { useState, useEffect } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import headerImg from "../assets/img/header-img.svg";
 import headerImg3 from "../assets/img/header5_img.png";
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import 'animate.css';
-import TrackVisibility from 'react-on-screen';
-import { HashLink } from 'react-router-hash-link';
+import globe_img from "../assets/img/globe_img.png"
+import { ArrowRightCircle } from "react-bootstrap-icons";
+import "animate.css";
+import TrackVisibility from "react-on-screen";
+import { HashLink } from "react-router-hash-link";
+// import Globe from "react-globe.gl";
 // import Reveal from 'react-reveal/Reveal';
-import Fade from 'react-reveal/Fade';
+import Fade from "react-reveal/Fade";
 
 // className={isVisible ? "animate__animated animate__fadeIn" : ""} line 70
 // className={isVisible ? "animate__animated animate__zoomIn" : ""} line 90
@@ -16,10 +18,10 @@ import Fade from 'react-reveal/Fade';
 export const Banner = () => {
   const [loopNum, setLoopNum] = useState(0);
   const [isDeleting, setIsDeleting] = useState(false);
-  const [text, setText] = useState('');
+  const [text, setText] = useState("");
   const [delta, setDelta] = useState(300 - Math.random() * 100);
   const [index, setIndex] = useState(1);
-  const toRotate = [ "Web Developer", "Frontend Developer", "Backend Developer" ];
+  const toRotate = ["Web Developer", "Frontend Developer", "Backend Developer"];
   const period = 2000;
 
   useEffect(() => {
@@ -27,38 +29,42 @@ export const Banner = () => {
       tick();
     }, delta);
 
-    return () => { clearInterval(ticker) };
-  }, [text])
+    return () => {
+      clearInterval(ticker);
+    };
+  }, [text]);
 
   const tick = () => {
     let i = loopNum % toRotate.length;
     let fullText = toRotate[i];
-    let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1);
+    let updatedText = isDeleting
+      ? fullText.substring(0, text.length - 1)
+      : fullText.substring(0, text.length + 1);
 
     setText(updatedText);
 
     if (isDeleting) {
-      setDelta(prevDelta => prevDelta / 2);
+      setDelta((prevDelta) => prevDelta / 2);
     }
 
     if (!isDeleting && updatedText === fullText) {
       setIsDeleting(true);
-      setIndex(prevIndex => prevIndex - 1);
+      setIndex((prevIndex) => prevIndex - 1);
       setDelta(period);
-    } else if (isDeleting && updatedText === '') {
+    } else if (isDeleting && updatedText === "") {
       setIsDeleting(false);
       setLoopNum(loopNum + 1);
       setIndex(1);
       setDelta(500);
     } else {
-      setIndex(prevIndex => prevIndex + 1);
+      setIndex((prevIndex) => prevIndex + 1);
     }
-  }
-
-  const handleScrollToConnect = () => {
-    window.location.href="https://drive.google.com/file/d/1H1T-DnWj3jdeGzY0ElCF7_gHhynl3i5H/view?usp=sharing";
   };
 
+  const handleScrollToConnect = () => {
+    window.location.href =
+      "https://drive.google.com/file/d/1H1T-DnWj3jdeGzY0ElCF7_gHhynl3i5H/view?usp=sharing";
+  };
 
   return (
     <section className="banner" id="home">
@@ -66,36 +72,47 @@ export const Banner = () => {
         <Row className="aligh-items-center">
           <Col xs={12} md={6} xl={7}>
             <TrackVisibility>
-              {({ isVisible }) =>
-              <div >
-                <Fade left cascade>
-                <span className="tagline">Welcome to my Portfolio!</span>
-                <h1>{`Hi! I'm Vedant`} <span className="txt-rotate" data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'><span className="wrap">{text}</span></span></h1>
-                  <p>
-                    Hi! I am Vedant Kesharia,I am a FullStack Web Developer, This is my portfolio website, I hope you like this website and my projects!
-                  </p>
+              {({ isVisible }) => (
+                <div>
+                  <Fade left cascade>
+                    <span className="tagline">Welcome to my Portfolio!</span>
+                    <h1>
+                      {`Hi! I'm Vedant`}{" "}
+                      <span
+                        className="txt-rotate"
+                        data-rotate='[ "Web Developer", "Web Designer", "UI/UX Designer" ]'
+                      >
+                        <span className="wrap">{text}</span>
+                      </span>
+                    </h1>
+                    <p>
+                      Hi! I am Vedant Kesharia,I am a FullStack Web Developer,
+                      This is my portfolio website, I hope you like this website
+                      and my projects!
+                    </p>
                   </Fade>
 
                   <Fade left cascade>
-                  <button onClick={handleScrollToConnect}>My Resume <ArrowRightCircle size={25} />
-                  </button>
+                    <button onClick={handleScrollToConnect}>
+                      My Resume <ArrowRightCircle size={25} />
+                    </button>
                   </Fade>
-                
-              </div>}
+                </div>
+              )}
             </TrackVisibility>
           </Col>
           <Col xs={12} md={6} xl={5}>
             <TrackVisibility>
-              {({ isVisible }) =>
-                <div >
+              {({ isVisible }) => (
+                <div>
                   {/* <img src={headerImg} alt="Header Img"/> */}
-                  <img src={headerImg3} alt="Header Img"/>
-                </div>}
+                  <img src={globe_img} alt="Header Img"/>
+                </div>
+              )}
             </TrackVisibility>
           </Col>
         </Row>
       </Container>
-      
     </section>
-  )
-}
+  );
+};
